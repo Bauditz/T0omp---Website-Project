969e7df0d07c6856836c4f4d039376eb0e42c6be
Lucas Gabriel Fregulia Bautitz <72616457+Bauditz@users.noreply.github.com>
2021-11-13 19:05:21 -0300

    T0omp - Web Project
    This is a test. My first project, any help is welcome.

---
+++ script.js
@@ -0,0 +1,29 @@
+var animation = bodymovin.loadAnimation({
+
+    container: document.getElementById('twitch'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Twitch.json'
+
+})
+
+var animation2 = bodymovin.loadAnimation({
+
+    container: document.getElementById('F1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'F1.json'
+
+})
+
+var animation3 = bodymovin.loadAnimation({
+
+    container: document.getElementById('French-fries'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'French-fries.json'
+
+})
